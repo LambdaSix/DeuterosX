df010491a8d9fce4e68a84fe69263c036a183aed
geddeth <geddeth@c7f5898e-e84a-0410-894e-9349b6e1bf0b>
2008-06-19 16:56:19 +0000

    Add a Deuteros.org config file for PhpBB3.

---
+++ www/forum/config-deuteros.php
@@ -0,0 +1,17 @@
+<?php
+// phpBB 3.0.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'mysqli';
+$dbhost = 'localhost';
+$dbport = '';
+$dbname = 'dbname';
+$dbuser = 'dbuser';
+$dbpasswd = 'dbpasswd';
+$table_prefix = 'phpbb3_';
+$acm_type = 'file';
+$load_extensions = '';
+
+@define('PHPBB_INSTALLED', true);
+// @define('DEBUG', true);
+// @define('DEBUG_EXTRA', true);
+?>
