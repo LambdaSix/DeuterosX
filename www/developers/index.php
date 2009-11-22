804b918a1dcd8cabfdc06533a4ce0147b2d7fba0
geddeth <geddeth@c7f5898e-e84a-0410-894e-9349b6e1bf0b>
2009-11-22 22:27:20 +0000

    Tweak the layout of the developers' page and add RSS feed for latest Wiki changes.

--- www/developers/index.php
+++ www/developers/index.php
@@ -23,48 +23,37 @@
 
     require_once($GLOBALS['DX_SITE_PATH'] . 'inc/simplepie/simplepie.inc');
     require_once($GLOBALS['DX_SITE_PATH'] . 'inc/header.php');
-    
+
 ?>
 
     <table>
         <tr>
-	        <td id="dxmaincolumn">
-			    <div style="width: 100%;">
-			        <p>
-			        This is the developers' page.
-			        </p>
-			        
-			        <p>
-                        <a href="http://sourceforge.net/projects/deuterosx">
-                            <img src="http://sflogo.sourceforge.net/sflogo.php?group_id=224652&amp;type=11" width="120" height="30" alt="Get Deuteros X at SourceForge.net. Fast, secure and Free Open Source software downloads" />
-                        </a>
-			        </p>
-			    </div>
-	        </td>
-	        
-	        <td id="dxrightcolumn">
-                <div class="dxrightbox">
-                
-                    <h2>IRC</h2>
-    
-                    <p>The developers gather in bi-weekly IRC meetings on the QuakeNet IRC servers in the #deuterosx.org channel.</p>
-			        <ul>
-			        <li><a href="chat/">IRC chat applet</a></li>
-			        <li><a href="deuterosx.org.log">IRC chat log</a></li>			        
-			        </ul>
-			        
-  
+            <td id="dxmaincolumn">
+                <div style="width: 100%;">
+                    <p>
+                    Would you like to join the <a href="<?php echo $GLOBALS['DX_SITE_URL'];?>wiki/DeuterosX:About">Deuteros X team</a>? Want to know what they are up to right now? This is the page to be looking at!
+                    </p>
+
                 </div>
-                
-                <div class="dxrightbox">
-                
+
+                <div style="width: 100%;">
+                    <h2>IRC</h2>
+
+                    <p>The developers gather in bi-weekly IRC meetings on the QuakeNet IRC servers in the #deuterosx.org channel.</p>
+                    <ul>
+                    <li><a href="chat/">IRC chat applet</a></li>
+                    <li><a href="deuterosx.org.log">IRC chat log</a></li>
+                    </ul>
+                </div>
+
+                <div style="width: 48%;">
                     <h2>Tickets</h2>
-                    
+
                     <ul><li><a href="http://sourceforge.net/apps/trac/deuterosx/">Browse Trac tickets</a></li></ul>
-                    
-			        <ul>
-			        
-<?php 
+
+                    <ul>
+
+<?php
     $tracfeed = new SimplePie();
     $tracfeed->set_feed_url('http://sourceforge.net/apps/trac/deuterosx/report/1?format=rss&USER=anonymous');
     $tracfeed->init();
@@ -72,25 +61,77 @@
 
     if ($tracfeed->data) {
         $tracitems = $tracfeed->get_items(0, 5);
-        foreach ($tracitems as $item) {        	
+        foreach ($tracitems as $item) {
             echo '<li>';
-            echo '<a target="_blank" href="' . $item->get_permalink() . '">' . $item->get_title() . '</a>'; 
+            echo '<a href="' . $item->get_permalink() . '">' . $item->get_title() . '</a>';
             echo '</li>';
         }
     }
 ?>
                     </ul>
-			    </div>
-			    
-			    <div class="dxrightbox">
-			        <h2>Subversion</h2>
-			        
-			        <ul>
-			            <li><a href="http://deuterosx.svn.sourceforge.net/viewvc/deuterosx/">Browse source</a></li>        
-			            <li><a href="http://sourceforge.net/mail/?group_id=224652">SVN commits mailing list</a></li>
-			        </ul>
-			        
-			        <ul>
+
+                    <div class="dxfeedtext"><a href="http://sourceforge.net/apps/trac/deuterosx/report/1?format=rss&amp;USER=anonymous">RSS feed</a></div>
+                </div>
+
+                <div style="width: 100%;">
+                    <p>
+                    Project development is supported by<br/>
+                        <a href="http://sourceforge.net/projects/deuterosx">
+                            <img src="http://sflogo.sourceforge.net/sflogo.php?group_id=224652&amp;type=11" width="120" height="30" alt="Get Deuteros X at SourceForge.net. Fast, secure and Free Open Source software downloads" />
+                        </a>
+                    </p>
+
+                </div>
+
+            </td>
+
+            <td id="dxrightcolumn">
+
+                <div class="dxrightbox">
+
+                    <h2>Wiki</h2>
+
+                    <ul><li><a href="<?php echo $GLOBALS['DX_SITE_URL'];?>wiki/Special:RecentChanges">Browse changes</a></li></ul>
+
+                    <ul>
+
+<?php
+    $wikifeed = new SimplePie();
+    $wikifeed->set_feed_url('http://deuterosx.org/mediawiki/index.php?title=Special:RecentChanges&feed=rss');
+    $wikifeed->init();
+    $wikifeed->handle_content_type();
+
+    if ($wikifeed->data) {
+        $wikiitems = $wikifeed->get_items(0, 5);
+        foreach ($wikiitems as $item) {
+            $author = $item->get_author();
+
+            echo '<li>';
+            echo $item->get_date('j M Y');
+            echo ' ';
+            echo '<strong>' . $author->Get_name() . '</strong>';
+            echo ' ';
+            echo '<a href="' . $item->get_permalink() . '">' . $item->get_title() . '</a>';
+            echo '</li>';
+        }
+    }
+?>
+                    </ul>
+
+                    <div class="dxfeedtext"><a href="http://deuterosx.org/mediawiki/index.php?title=Special:RecentChanges&amp;feed=rss">RSS feed</a></div>
+
+                </div>
+
+
+                <div class="dxrightbox">
+                    <h2>Subversion</h2>
+
+                    <ul>
+                        <li><a href="http://deuterosx.svn.sourceforge.net/viewvc/deuterosx/">Browse source</a></li>
+                        <li><a href="http://sourceforge.net/mail/?group_id=224652">SVN commits mailing list</a></li>
+                    </ul>
+
+                    <ul>
 <?php
 
     $svnfeed = new SimplePie();
@@ -101,21 +142,27 @@
     if ($svnfeed->data) {
         $svnitems = $svnfeed->get_items(0, 5);
         foreach ($svnitems as $item) {
+            $descArray = explode(' ', $item->get_description());
+            $commiterName = $descArray[16];
+
             echo '<li>';
             echo $item->get_date('j M Y');
             echo ' ';
-            echo '<a target="_blank" href="' . $item->get_permalink() . '">' . $item->get_title() . '</a>'; 
+            echo $commiterName;
+            echo ' ';
+            echo '<a href="' . $item->get_permalink() . '">' . $item->get_title() . '</a>';
             echo '</li>';
         }
     }
 ?>
-				    </ul>
-				
-			    </div>
-	        
-	        </td>
+                    </ul>
+
+                    <div class="dxfeedtext"><a href="http://cia.vc/stats/project/DeuterosX/.rss">RSS feed</a></div>
+                </div>
+
+            </td>
         </tr>
-    </table>  
+    </table>
 
 <?php
     require_once($GLOBALS['DX_SITE_PATH'] . 'inc/footer.php');
